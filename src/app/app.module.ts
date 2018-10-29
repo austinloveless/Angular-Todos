6a78bd1fe26663bb6b897191b73f57d114fb06e5
austinloveless <austinloveless5171@gmail.com>
2018-10-29 12:23:15 -0600

    Todos, Add, get, remove todos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { TodoInputComponent } from "./components/todo-input/todo-input.component";
+import { TodoService } from "./services/todo.service";
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [AppComponent, HeaderComponent, TodoInputComponent, TodoItemComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [TodoService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
